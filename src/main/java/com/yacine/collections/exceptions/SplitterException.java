d2cdd5613bf4465655aac526af69a1c23796d1d6
ymeddah <my.meddah@gmail.com>
2017-11-18 22:02:22 +0100

    SplitterException class for checked exceptions

---
+++ src/main/java/com/yacine/collections/exceptions/SplitterException.java
@@ -0,0 +1,9 @@
+package com.yacine.collections.exceptions;
+
+public class SplitterException extends Exception {
+
+    public SplitterException(String message) {
+        super(message);
+    }
+
+}
